4948bc4e1859efa751397508bd4a4897956856ff
Mikhail Gerasimov <mgerasim@inbox.ru>
2018-09-22 06:46:52 +1000

    Добавлен интерфейс отправки

---
+++ DocmanMobile/Services/DataSendProvider/DataSend.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace DocmanMobile.Services.DataSendProvider
+{
+    public class DataSend
+    {
+        public DataSend()
+        {
+        }
+    }
+}
---
+++ DocmanMobile/Services/DataSendProvider/IDataSend.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace DocmanMobile.Services.DataSendProvider
+{
+    public class IDataSend
+    {
+        public IDataSend()
+        {
+        }
+    }
+}
