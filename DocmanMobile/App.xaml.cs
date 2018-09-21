c5c1cecfc36832e6caeea1fd23f9cdb1bdeed619
Mikhail Gerasimov <mgerasim@inbox.ru>
2018-09-22 07:15:09 +1000

    Добавлен интерфейс отправки

--- DocmanMobile/App.xaml.cs
+++ DocmanMobile/App.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using DocmanMobile.Pages;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -11,7 +12,7 @@ namespace DocmanMobile
         {
             InitializeComponent();
 
-            MainPage = new MainPage();
+            MainPage = new CallPage();
         }
 
         protected override void OnStart()
---
+++ DocmanMobile/Pages/CallPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace DocmanMobile.Pages
+{
+    public partial class CallPage : ContentPage
+    {
+        public CallPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
