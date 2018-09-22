631c22e3e2f09887fa889afeaec214efcdc0525b
Mikhail Gerasimov <mgerasim@inbox.ru>
2018-09-23 00:08:40 +1000

    Views

---
+++ DocmanMobile/Views/PatientView.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using Xamarin.Forms;
+
+namespace DocmanMobile.Views
+{
+    public partial class PatientView : ContentView
+    {
+        public PatientView()
+        {
+            InitializeComponent();
+        }
+    }
+}
